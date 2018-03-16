d93f8cc90fde468f0708418cee44ec3ab015029a
Sandy Green <green7786@comcast.net>
2018-03-16 17:41:31 -0600

    worked on layout/and js

--- js/main.js
+++ js/main.js
@@ -1,8 +1,13 @@
 
 
+//  VARIABLES
+const user = document.querySelector('.user-search');
+const message = document.querySelector('.user-message');
+const closeBox = document.querySelector('.close2');
 
-//
-
+const sendModal = document.getElementById('myModal');
+const button = document.getElementById("myBtn");
+const modalMessage = document.querySelector('.modal-message');
 
 
 
@@ -74,10 +79,8 @@ var myLineChart = new Chart(ctx, {
         });
 
 
-
       //DONUT CHART
-
-      var ctx = document.getElementById("myDoughnutChart");
+  var ctx = document.getElementById("myDoughnutChart");
       var myDoughnutChart = new Chart(ctx, {
         type: 'doughnut',
         data: {
@@ -112,7 +115,7 @@ var myLineChart = new Chart(ctx, {
           });
 
 
-/*-------------MODAL POP-UP --------*/
+/*-------------ALERT MODAL POP-UP --------*/
 // Get the modal
 var modal = document.getElementById('myModal');
 
@@ -140,5 +143,31 @@ span.onclick = function() {
 window.onclick = function(event) {
     if (event.target == modalContent) {
         modalContent.style.display = "none";
+    }
+}
+
+
+/*------------SEND BUTTON MODAL POP-UP --------*/
+// When the user clicks the button, open the modal
+button.onclick = function() {
+    event.preventDefault();
+  if (user.value.length === 0 || message.value.length === 0)  {
+    modalMessage.innerHTML = "Please fill out required fields!";
+    sendModal.style.display = "block";
+  }  else  {
+    modalMessage.innerHTML = "Your message has been sent!";
+    sendModal.style.display = "block";
+  }
+}
+
+// When the user clicks on <span> (x), close the modal
+closeBox.addEventListener('click',  function() {
+    modal.style.display = "none";
+});
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+    if (event.target == modal) {
+        modal.style.display = "none";
     }
 }
