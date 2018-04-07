0aec3e858394971fca4fe213235f6a800e1aaafb
Sandy Green <green7786@comcast.net>
2018-04-07 17:45:52 -0600

    wrote js to populate and create cards

--- main.js
+++ main.js
@@ -1,121 +1,79 @@
+$(document).ready(function () {
 
-
-
-
-var rugAPI = "https://randomuser.me/api/?results=12&inc=name,email,location,phone,address,dob,picture&nat=US"
-
-// ------GET THE JSON DATA FROM RANDOM USER GENERATOR
-var member = new XMLHttpRequest();
-member.onreadystatechange = function() {
- if(member.readyState === 4) {
-        var data = JSON.parse(member.responseText);
-        var txt = "";
-
-        function memberInfo (){
-        var txt =  '<div id="grid">';
-        for (let i = 0; i <data.results.length; i+=1) {
-          txt +=  '<div id="cards">';
-          txt += '<img src="'+ data.results[i].picture.medium + '" class="avatar">';
-          txt +=  '<ul class="card-content">';
-          txt += '<li id="name">' + (data.results[i].name.first.substring(0,1).toUpperCase() + data.results[i].name.first.substring(1)) + "  " +
-                        (data.results[i].name.last.substring(0,1).toUpperCase() + data.results[i].name.last.substring(1)) + '</li>';
-          txt +=   '<li id="email">' + data.results[i].email + '</li>';
-          txt +=   '<li id="city">' + data.results[i].location.city.substring(0,1).toUpperCase() + data.results[i].location.city.substring(1) + '</li>';
-          txt +=  '</ul>';
-          txt += '</div>';
-          }
-          txt += '</div>';
-
-          document.getElementById('directory-container').innerHTML = txt;
-        }
-        memberInfo();
- }
-};
-member.open('GET',  rugAPI);
-member.send();
-
-function modalMemberInfo() {
-
-}
-
-
- //VARIABLES
-const user = document.querySelector('.user-search');
-const message = document.querySelector('.user-message');
-const closeBox = document.querySelector('.close2');
-const button = document.getElementById("cards");
-const modalMessage = document.querySelector('.modal-message');
+//VARIABLES
 const modal = document.getElementById('myModal');
 
 
-
-/*-------------ALERT MODAL POP-UP --------*/
-//Get the modal
-// var modal = document.getElementById('myModal');
+// function displayModal(person) {
 //
-// var modalContent = document.getElementById('modal-content');
 //
-// // Get the button that opens the modal
-// var btn = document.getElementById("myBtn");
+//     var txt =  '<div id="myModal">';
+//       txt += '<div class="modal">';
+//       txt +=  '<div id="modal__contents">';
+//       txt += '<span class="close">&times;</span>';
+//       txt += '<img src="'+ img + '" class="avatar">';
+//       txt +=  '<ul class="modal-card-content">';
+//       txt += '<li id="name">' + (person.name.first.substring(0,1).toUpperCase() + person.name.first.substring(1)) + "  " +
+//                     (person.name.last.substring(0,1).toUpperCase() + person.name.last.substring(1)) + '</li>';
+//       txt +=   '<li id="email">' + person.email + '</li>';
+//       txt +=   '<li id="city">' + person.location.city.substring(0,1).toUpperCase() + person.location.city.substring(1) + '</li>';
+//       txt +=  '</ul>';
+//       txt += '<ul class="employee-info">';
+//        txt += '<li id="phone">' + person.phone + ' </li>'
+//       txt += '<li id="address"> ' + person.location.street +" " + person.location.state + " " + person.location.postcode + ' </li>'
+//       txt += '<li id="birthday">' + "Birthday: "  + fullbday  +' </li>'
+//       txt +=  '</ul>';
+//       txt += '</div>';
+//       $('#myModal').html(txt)
 //
-// // Get the <span> element that closes the modal
-// var span = document.getElementsByClassName("close")[0];
-//
-// // // When the user loads the page, open the modal
-// window.onload = function(event) {
-//     if(event.target == modalContent) {
-//       modalContent.style.display = "block";
-//     }
-// }
-//
-// // When the user clicks on <span> (x), close the modal
-// span.onclick = function() {
-//     modalContent.style.display = "none";
-// }
-//
-// // When the user clicks anywhere outside of the modal, close it
-// window.onclick = function(event) {
-//     if (event.target == modalContent) {
-//         modalContent.style.display = "none";
-//     }
+//       //  When the user clicks on <span> (x), close the modal
+//     $('#modal__contents').on('click',  function() {
+//             $('#myModal').remove();
+//     });
 // }
 
-var wrapper = document.getElementById('directory-container');
 
-/*------------SEND BUTTON MODAL POP-UP --------*/
-// When the user clicks the members card, open the modal
-$(document).ready(function(e){
-$(wrapper).on('click', ".card-content", function(event){
-  modal.style.display = "block";
 
-  // var txt =  '<div id="grid">';
-  // for (let i = 0; i <data.results.length; i+=1) {
-  //   txt +=  '<div id="cards">';
-  //   txt += '<img src="'+ data.results[i].picture.medium + '" class="avatar">';
-  //   txt +=  '<ul class="card-content">';
-  //   txt += '<li id="name">' + (data.results[i].name.first.substring(0,1).toUpperCase() + data.results[i].name.first.substring(1)) + "  " +
-  //                 (data.results[i].name.last.substring(0,1).toUpperCase() + data.results[i].name.last.substring(1)) + '</li>';
-  //   txt +=   '<li id="email">' + data.results[i].email + '</li>';
-  //   txt +=   '<li id="city">' + data.results[i].location.city.substring(0,1).toUpperCase() + data.results[i].location.city.substring(1) + '</li>';
-  //   txt +=  '</ul>';
-  //   txt += '</div>';
-  //   }
-  //   txt += '</div>';
-  //
-  //   document.getElementById('directory-container').innerHTML = txt;
 
-    // alert('clicked');
-});
-});
+var rugAPI = "https://randomuser.me/api/?results=12&inc=name,email,location,phone,dob,picture&nat=US"
 
-// When the user clicks on <span> (x), close the modal
-closeBox.addEventListener('click',  function() {
-    modal.style.display = "none";
-});
+// ------GET THE JSON DATA FROM RANDOM USER GENERATOR
+$.getJSON(rugAPI,  function(data) {
+  var data = data.results;
+    console.log(data);
+  // ----------Create Employee Directory
+    var txt =  '<div id="grid">';
+    for (let i = 0; i <data.length; i+=1) {
 
-// When the user clicks anywhere outside of the modal, close it
-window.onclick = function(event) {
-    if (event.target == modal) {
-        sendModal.style.display = "none";
-    }
-}
+    var fullname = (data[i].name.first.substring(0,1).toUpperCase() + data[i].name.first.substring(1)) + "  " +  (data[i].name.last.substring(0,1).toUpperCase() + data[i].name.last.substring(1));
+    var img = data[i].picture.medium;
+    var email =  data[i].email;
+    var city = data[i].city;
+    var phone = data[i].phone;
+    var address =  data[i].location.street +" " + data[i].location.state + " " + data[i].location.postcode;
+    var fullbday = data[i].dob;
+    fullbday = fullbday.substring(0, fullbday.length-8);
+
+
+
+      /*------------MEMBER  MODAL POP-UP --------*/
+      $(document).on('click ',  '.cards', function(e) {
+        // var clickedCard = e.target;
+
+             $('#myModal').show();
+      });
+
+      txt +=  '<div class="cards">';
+      txt += '<img src="'+ img + '" class="avatar">';
+      txt +=  '<ul class="card-content">';
+      txt += '<li id="name">' + fullname + '</li>';
+      txt +=   '<li id="email">' + email + '</li>';
+      txt +=   '<li id="city">' + city + '</li>';
+      txt +=  '</ul>';
+      txt += '</div>';
+      }
+      txt += '</div>';
+    $('#directory-container').html(txt)
+
+  }); // end get JSON
+}); //end ready
