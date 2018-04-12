b620b5a3b61993f77065ebc79ee65252ab9af416
Sandy Green <green7786@comcast.net>
2018-04-12 13:16:44 -0600

    formatted json data and updated arrows css

--- js/main.js
+++ js/main.js
@@ -7,18 +7,20 @@ const search = document.getElementById('search');
 let employees= [];
 let selectedIndex = 0;
 
+//Capitalize letters
+const titleCase = function (str) {
+  str = str.split(' ');
+  for (var i = 0; i < str.length; i++) {
+    str[i] = str[i].charAt(0).toUpperCase() + str[i].slice(1);
+  }
+  return str.join(' ');
+}
 
 
 function memberModal(i){
  selectedIndex = employees.indexOf(i);
 
- // Open the modal
-      // let employee = employees[index];
-      let fullbday = i.dob;
-      fullbday = fullbday.substring(0, fullbday.length-8);
-
       $('#myModal').show();
-
 
       let txt =  '<div id="myModal" >';
       txt += '<div class="modal">';
@@ -26,17 +28,16 @@ function memberModal(i){
       txt += '<span class="close" aria-label="close">&times;</span>';
       txt += '<img src="'+ i.picture.large + '" class="avatar">';
       txt +=  '<ul class="modal-card-content">';
-      txt += '<li id="name">' + (i.name.first.substring(0,1).toUpperCase() + i.name.first.substring(1)) + "  " +
-                    (i.name.last.substring(0,1).toUpperCase() + i.name.last.substring(1)) + '</li>';
+      txt += '<li id="name">' + titleCase (i.name.first) + "  " + titleCase(i.name.last);
       txt +=   '<li id="email">' + i.email + '</li>';
       txt +=   '<li id="city">' + i.location.city.substring(0,1).toUpperCase() + i.location.city.substring(1) + '</li>';
       txt +=  '</ul>';
       txt += '<ul class="employee-info">';
-       txt += '<li id="phone">' + i.cell + ' </li>'
-      txt += '<li id="address"> ' + i.location.street +" " + i.location.state + " " + i.location.postcode + ' </li>'
-      txt += '<li id="birthday">' + "Birthday: "  + fullbday  +' </li>'
-      txt += '<span class="arrow-right"  data-index="' + i + '" ></span>';
-      txt += '<span class="arrow-left" ></span';
+       txt += '<li id="phone">' + i.cell.replace("-", ' '); ' </li>'
+      txt += '<li id="address"> ' + titleCase(i.location.street) +" " + titleCase(i.location.state) + " " + i.location.postcode + ' </li>'
+      txt += '<li id="birthday">' + "Birthday: "  +  new Date(i.dob).toLocaleDateString()  +' </li>'
+      txt += '<span class="arrow-right"  data-index="' + i + '" >&#8250;</span>';
+      txt += '<span class="arrow-left" >&#8249;</span';
       txt +=  '</ul>';
       txt += '</div>';
       $('#myModal').html(txt)
@@ -64,7 +65,6 @@ if (selectedIndex === 0) {
   }); // End
 
 
-
 // ------GET THE JSON names FROM RANDOM USER GENERATOR
 
 var rugAPI = "https://randomuser.me/api/?results=12&nat=US&inc=name,email,location,cell,dob,picture"
@@ -76,12 +76,13 @@ $.getJSON(rugAPI,  function(data) {
 
     var txt =  '<div id="grid">';
     for (let i = 0; i <data.length; i+=1) {
-        var fullname = (data[i].name.first.substring(0,1).toUpperCase() + data[i].name.first.substring(1)) + "  " +  (data[i].name.last.substring(0,1).toUpperCase() + data[i].name.last.substring(1));
+        // var fullname = (data[i].name.first.substring(0,1).toUpperCase() + data[i].name.first.substring(1)) + "  " +  (data[i].name.last.substring(0,1).toUpperCase() + data[i].name.last.substring(1));
+        var fullname = titleCase(data[i].name.first) + "  " +  titleCase(data[i].name.last);
         var   img = data[i].picture.large;
         var   email =  data[i].email;
-        var  city = data[i].location.city.substring(0,1).toUpperCase()  + data[i].location.city.substring(1) ;
+        var  city = titleCase(data[i].location.city);
         var  phone = data[i].cell;
-        var   address =  data[i].location.street + " " + data[i].location.state + " " + data[i].location.postcode;
+        var   address =  titleCase(data[i].location.street) + " " + titleCase(data[i].location.state) + " " + data[i].location.postcode;
 
         /*------------MEMBER  MODAL POP-UP --------*/
 
@@ -106,10 +107,6 @@ $.getJSON(rugAPI,  function(data) {
   $('#directory-container').html(txt)
 
 
-
-
-
-
   // Search function
   $("#search").keyup(function(){
       var value = $(this).val().toUpperCase();
@@ -118,7 +115,5 @@ $.getJSON(rugAPI,  function(data) {
       });
   }); // End Search Function
 
-
-
-  }); // end get JSON
+}); // end get JSON
 }); //end ready
